9a7d8b5d05b632750ee035dd50c0ef102f454f29
Jared Page <jared.p@dronesec.com>
2023-03-25 13:56:47 +1000

    Added meta tags, images and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import learnhowowl from "./static/images/learnhowowl.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"banner flex justify-center items-center w-full h-screen"}>
+          <div className={"w-full max-w-4xl"}>
+              <div className={"flex justify-around py-5 gap-10"}>
+                  <div className={"flex flex-col justify-center"}>
+                      <div className={"flex flex-col gap-5"}>
+                          <div>
+                              <h1 className={"title"}>Learn How<span className={"dots"}>...</span>soon</h1>
+                              <div className={"subtitle"}>Knowledge at your fingertips</div>
+                          </div>
+                          <div className={"description"}>
+                              Get ready to explore thousands of different articles and learn something new. Coming soon.
+                          </div>
+                      </div>
+                  </div>
+                  <img src={learnhowowl} alt="Learn How to Do Things with Mr. Owl | Learn How"/>
+              </div>
+          </div>
+      </div>
   );
 }
 
